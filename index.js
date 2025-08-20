217c2f730fc3386d8ec2cede1d908b8af9d0acdb
Biswadip Bhowmick <biswadip.bhowmick@gushwork.ai>
2025-08-20 19:03:55 +0530

    feat: add prod logs

--- index.js
+++ index.js
@@ -10,3 +10,6 @@ console.log("Hello World");
 console.log("Hello World");
 console.log("Hello World");
 console.log("Hello World");
+
+// adding a comment and few logs here
+console.log("Hello World");
