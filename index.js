0cc1fcc154c369031b0410d9b1bc6bf27eccd386
Biswadip Bhowmick <tobiswadip@gmail.com>
2025-08-21 12:28:54 +0530

    feat: add more logs to e reverted (#13)

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ console.log("Hello World");
 
 console.log("Hello World");
 console.log("Hello World");
+
+//to be reverted
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
